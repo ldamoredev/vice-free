1ce0811ba84dcd01625f5010aa49111ac0d7c641
ldamoredev <lauti.damore@gmail.com>
2024-04-02 01:50:03 -0300

    safe area

--- src/ui/RootView.tsx
+++ src/ui/RootView.tsx
@@ -5,16 +5,19 @@ import { StatusBar } from 'react-native'
 import { NavigationView } from './navigation/NavigationView'
 import { AppInitialization } from './initializers/AppInitialization/AppInitialization'
 import { UnhandledErrorProvider } from '@nbottarini/react-presenter'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export const RootView: FC<Props> = ({ context }) => {
     return (
         <AppContext.Provider value={context}>
-            <UnhandledErrorProvider onUnhandledError={context.onUnhandledError}>
-                <AppInitialization>
-                    <StatusBar barStyle="dark-content" backgroundColor="rgba(0,0,0,0)" translucent />
-                    <NavigationView navigator={context.navigator} />
-                </AppInitialization>
-            </UnhandledErrorProvider>
+            <SafeAreaProvider>
+                <UnhandledErrorProvider onUnhandledError={context.onUnhandledError}>
+                    <AppInitialization>
+                        <StatusBar barStyle="dark-content" backgroundColor="rgba(0,0,0,0)" translucent />
+                        <NavigationView navigator={context.navigator} />
+                    </AppInitialization>
+                </UnhandledErrorProvider>
+            </SafeAreaProvider>
         </AppContext.Provider>
     )
 }
